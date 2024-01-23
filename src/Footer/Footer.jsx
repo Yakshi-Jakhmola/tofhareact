aea1af153e9cc3b41ecccba2d1c4efeefc2df170
Yogesh Negi <113903081+itsyogeshnegi@users.noreply.github.com>
2024-01-23 22:41:20 +0530

    products

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -24,10 +24,10 @@ const Footer = () =>{
                                 </div>
                                 <div className="footer-social-link">
                                     <ul>
-                                        <li><a href=""><i className="fa-brands fa-facebook-f"></i></a></li>
-                                        <li><a href=""><i className="fa-brands fa-instagram"></i></a></li>
-                                        <li><a href=""><i className="fa-brands fa-linkedin-in"></i></a></li>
-                                        <li><a href=""><i className="fa-brands fa-x-twitter"></i></a></li>
+                                        <li><a><i className="fa-brands fa-facebook-f"></i></a></li>
+                                        <li><a><i className="fa-brands fa-instagram"></i></a></li>
+                                        <li><a><i className="fa-brands fa-linkedin-in"></i></a></li>
+                                        <li><a><i className="fa-brands fa-x-twitter"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
